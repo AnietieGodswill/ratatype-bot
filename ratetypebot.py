3bfc1672d12b3fb11175bf0ad3a763da86e0aad0
Nishant Tiwari <56861578+dx4iot@users.noreply.github.com>
2020-02-12 03:28:28 +0530

    Update ratetypebot.py

--- ratetypebot.py
+++ ratetypebot.py
@@ -1,33 +1,44 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import pyautogui
 import pyperclip
 import time
-# pyperclip to paste the content from the clipboard
-browser = webdriver.Chrome("path") #change this path to your chromedriver path
-username = '__email__' #enter your email
-password = '__password__' #enter your password
+browser = webdriver.Chrome("C:/Users/Aaditya/Downloads/chromedriver")#change this path to your chromedriver path
+
+print('RataType Bot (ratatype.com)')
+print('Created By : dx4iot')
+username = input("Enter Email: ")#enter your email
+password = input("Enter Password: ") #enter your password
 browser.get('https://www.ratatype.com/login/')
 browser.find_element_by_id('email').send_keys(username)
-browser.find_element_by_id('password').send_keys(password)
-time.sleep(15)
+browser.find_element_by_id('password').send_keys(password,Keys.RETURN)
+time.sleep(10)
 browser.get("https://www.ratatype.com/typing-test/test/")
-time.sleep(3)
-pyautogui.typewrite(['enter'])
-start = input("Press y to start \n")
 while(True):
-    if(start == 'y'):
-        copypath = (browser.find_elements_by_class_name("mainTxt"))
-        for post in copypath:
-            print(post.text) 
+    browser.find_element_by_xpath("//*[contains(text(),'start typing now')]").click()
+    start = input("Press s to start: ")
+    if(start == 's'):
+        copyPath = (browser.find_elements_by_class_name("mainTxt"))
+        for post in copyPath:
+            print(post.text)
+            copyTo = post.text    
+        pyperclip.copy(copyTo)
         time.sleep(10)
         twrite = pyperclip.paste()
-        inp_time = float(input("Enter time"))
+        inp_time = float(input("Enter time: "))
         time.sleep(5)
+        pyautogui.FAILSAFE=False
         pyautogui.typewrite(twrite,float(inp_time))
         time.sleep(5)
-        end = input("Press y to start again\n")
+        browser.find_element_by_xpath('//*[@id="complete"]/div[2]/div/div[2]/div/button').click()
+        end = input("continue? (y or n)")
         if(end == 'y'):
             continue
+        
+
+        
+
+
 
         
 
