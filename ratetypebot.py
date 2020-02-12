4a624303f9d51e4d868c64eede5f300082addfc8
Nishant Tiwari <56861578+dx4iot@users.noreply.github.com>
2020-02-12 12:00:07 +0530

    Update ratetypebot.py

--- ratetypebot.py
+++ ratetypebot.py
@@ -3,10 +3,9 @@ from selenium.webdriver.common.keys import Keys
 import pyautogui
 import pyperclip
 import time
-browser = webdriver.Chrome("C:/Users/Aaditya/Downloads/chromedriver")#change this path to your chromedriver path
+browser = webdriver.Chrome("C:/chromedr/chromedriver")#change this path to your chromedriver path
 
 print('RataType Bot (ratatype.com)')
-print('Created By : dx4iot')
 username = input("Enter Email: ")#enter your email
 password = input("Enter Password: ") #enter your password
 browser.get('https://www.ratatype.com/login/')
@@ -16,6 +15,7 @@ time.sleep(10)
 browser.get("https://www.ratatype.com/typing-test/test/")
 while(True):
     browser.find_element_by_xpath("//*[contains(text(),'start typing now')]").click()
+
     start = input("Press s to start: ")
     if(start == 's'):
         copyPath = (browser.find_elements_by_class_name("mainTxt"))
@@ -30,11 +30,18 @@ while(True):
         pyautogui.FAILSAFE=False
         pyautogui.typewrite(twrite,float(inp_time))
         time.sleep(5)
-        browser.find_element_by_xpath('//*[@id="complete"]/div[2]/div/div[2]/div/button').click()
+        try:
+            browser.find_element_by_xpath('//*[@id="complete"]/div[2]/div/div[2]/div/button').click()
+        except:
+            browser.find_element_by_xpath("//*[contains(text(), 'TAKE A TYPING TEST')]").click()
         end = input("continue? (y or n)")
         if(end == 'y'):
             continue
         
+
+        
+
+
 
         
 
